4ec4dff5c615d4e80388e5cf55b08dd53bbd0d58
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-06-22 19:02:50 +0000

    REDIS

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Using Redis as a simple cache'''
+
+
+import redis
+from typing import Union
+from uuid import uuid4
+
+
+class Cache:
+    """A class that Caches using Redis"""
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[bytes, str, int, float]) -> str:
+        """stores and returns data"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
